949a47e005b98f1db0077160ca0254d20b5e37c9
Carlos Daniel <45631849+CarlossDaniell@users.noreply.github.com>
2018-12-05 12:59:33 -0200

    Add files via upload

---
+++ Correção_Prova/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+{
+namespace Correcao
+
+
+
+
+    private double altura;
+
+    public IMC()
+    {
+    this.peso = 0.0;
+    this.altura = 0.0;
+    }
+
+    public IMC(double peso, double altura)
+    {
+    this.peso = peso;
+    this.altura = altura;
+    }
+
+    public double Calcular(double peso, double altura)
+    {
+    return peso / (altura * altura);
+
+    }
+}
---
+++ Correção_Prova/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Correção_Prova
+{
+    class Program
+    {
+        //Questão 1
+        static int Maior(int a, int b)
+        {
+            if (a > b)
+                return a;
+
+            else
+                return b;
+            static void Main(string[] args)
+        {
+
+        }
+
+
+        //Questão 3
+        static bool Par (int numero)
+        {
+            int resto = numero % 2;
+            if (resto == 0)
+                return true;
+
+            else
+                return false;
+        Console.ReadKey();
+        }
+        
+    }
+}
